8fd518042e73615744876c58b22003220d47347b
David Wertheimer <dwertheimer@users.noreply.github.com>
2025-04-02 23:36:52 -0700

    fix deeplink nav issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,7 +21,7 @@ export function Layout({
 
   return (
     <SectionProvider sections={sections}>
-      <div className="h-full lg:ml-72 xl:ml-80">
+      <div className="relative flex h-full">
         <motion.header
           layoutScroll
           className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
@@ -34,9 +34,11 @@ export function Layout({
             <Navigation className="hidden lg:mt-10 lg:block" />
           </div>
         </motion.header>
-        <div className="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
-          <main className="flex-auto">{children}</main>
-          <Footer />
+        <div className="flex-1 lg:ml-72 xl:ml-80">
+          <div className="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
+            <main className="flex-auto">{children}</main>
+            <Footer />
+          </div>
         </div>
       </div>
     </SectionProvider>
