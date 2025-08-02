a2266bddf0581fcdd5a2af1e40dcb75ebdabeb37
David Wertheimer <dwertheimer@users.noreply.github.com>
2025-08-02 15:35:41 -0700

    Notes on QuickNotes

--- src/data/faq-data.js
+++ src/data/faq-data.js
@@ -6,11 +6,7 @@ export const faqData = [
   {
     id: 'newNoteTitle-not-working',
     question:
-      'I am inserting a template in a blank note using the "Insert Template" button (or the `/insert` or `/append` command) and have a newNoteTitle field in my template, and the template is being rendered but the title is not being set.',
-    answer: `If you first create a new note and then try to insert a template using insert or append (or the "Insert Template" button), any newNoteTitle already been created in the default folder or wherever you created it. If you want to set the title using the newNoteTitle field:
-
-The easiest/quickest way is to simply use /new note from template command. This is the best way to create a note because it can read its title from newNoteTitle and can get the folder to put it in from the folder field in the template.
-
-Otherwise, you can set your title in the first line of the template with a # your title`,
+      'I created a blank note, added some content, then tried to insert a template using the "Insert Template" button (or the `/insert` or `/append` command) but the newNoteTitle field in the template is not changing the title of the note.',
+    answer: `Once a note has a title (and/or some content), then newNoteTitle is ignored. If you want newNoteTitle to be used, you must first create the note using the \`/New Note from Template\` command or insert the template into a blank note.`,
   },
 ]
